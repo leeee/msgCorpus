7fe279b94ba4fa447cf8b7151f76e3a4dd25d8a3
Le Wei <le.giantsquid.wei@gmail.com>
2013-10-16 16:48:17 -0400

    n-factor 1 markov chain

---
+++ le-markov.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3, nltk, random, sys
+
+conn = sqlite3.connect('messages.db')
+
+c = conn.cursor()
+
+# dictionary of words to followup words and probabilities?
+dictionary = {}
+nonword = '\n'
+w1 = nonword
+for row in c.execute('SELECT text FROM message WHERE is_from_me=1'):
+	if row[0] != None:
+		myString = row[0].encode('utf-8')
+		for word in myString.split():
+			dictionary.setdefault(w1,[]).append(word)
+			w1 = word;
+		dictionary.setdefault(w1, []).append(nonword)
+		w1 = nonword
+
+maxwords = 100
+
+for i in xrange(maxwords):
+    newword = random.choice(dictionary[w1])
+    if newword == nonword:
+    	sys.stdout.write('\n')
+    	sys.exit()
+    sys.stdout.write(newword + ' ')
+    w1 = newword
